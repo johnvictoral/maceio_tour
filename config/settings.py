38f5c794a4629f621be2ec1ea5dd3584f2c7dc10
John Victor <johnvictor.al@gmail.com>
2026-03-31 20:15:26 -0300

    voltando o normals

--- config/settings.py
+++ config/settings.py
@@ -156,8 +156,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/admin/login/' # Usando a tela de login do admin do Django, que é mais fácil
 # Mude isso:
-LOGIN_REDIRECT_URL = 'painel_parceiro' # Se o login der certo e não soubermos para onde ir, vai para a Home
-LOGIN_URL = 'login_parceiro' # URL padrão de login
+LOGIN_REDIRECT_URL = '/dashboard/' # Se o login der certo e não soubermos para onde ir, vai para a Home
+LOGIN_URL = '/admin/login/' # URL padrão de login
 LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = 'static/'
